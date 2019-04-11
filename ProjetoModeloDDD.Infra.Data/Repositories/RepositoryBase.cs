fa3dbb4d714379901fe0320e59cc2a5be16caee1
Yendis Gomes <yendis.gomes@hotmail.com>
2019-04-10 23:51:03 -0300

    updates

--- ProjetoModeloDDD.Infra.Data/Repositories/RepositoryBase.cs
+++ ProjetoModeloDDD.Infra.Data/Repositories/RepositoryBase.cs
@@ -4,8 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ProjetoModeloDDD.Infra.Data.Repositories
 {
